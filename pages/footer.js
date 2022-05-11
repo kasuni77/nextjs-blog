b4358f871bd639dce10a55965d99d9222c249517
kasuni77 <kasunimadhushika77@gmail.com>
2022-05-12 02:04:59 +0530

    create footer.js

---
+++ pages/footer.js
@@ -0,0 +1,13 @@
+export default function footer(){
+    return(
+        <div>
+            <div className="container" >
+                <footer className="py-3 my-4" style={{marginTop:"5px"}}>
+                    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                        <p className="text-center text-muted">© 2022 Shopzilla Company, Inc</p>
+                    </ul>
+                </footer>
+            </div>
+        </div>
+    );
+}
